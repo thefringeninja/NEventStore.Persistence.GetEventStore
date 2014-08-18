6fc6b82dfdf9ed9cda721b90d3321591cd498783
thefringeninja <joao.paul@braganca.name>
2014-08-18 11:18:27 +0200

    targeting 4.5.1

--- build/default.ps1
+++ build/default.ps1
@@ -34,7 +34,7 @@ task UpdateVersion {
 
 task Compile {
 	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /t:Clean }
-	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5 /p:OutDir=$output_directory }
+	exec { msbuild /nologo /verbosity:quiet $sln_file /p:Configuration=$target_config /p:TargetFrameworkVersion=v4.5.1 /p:OutDir=$output_directory }
 }
 
 task Test -precondition { $runPersistenceTests } {
